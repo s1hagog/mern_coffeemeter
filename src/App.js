db790245289a527721024942a9297b9d6577350b
s1hagog <moshakalex@gmail.com>
2020-01-29 22:55:38 -0800

    adding imports

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import {BroswerRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // import './App.css';
 
+import CreateProject from './components/create-project.component';
+import CreateUser from './components/create-user.component';
+import EditProject from './components/edit-project.component';
+import ProjectsList from './components/projects-list.component';
+
 function App() {
   return (
     <Router>
-      <Route path="/" exact component={ProjectList} />
+      <Route path="/" exact component={ProjectsList} />
       <Route path="/edit/:id" component={EditProject} />
       <Route path="/create" component={CreateProject} />
       <Route path="/user" component={CreateUser} />
