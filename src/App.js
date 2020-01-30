eec25e9485746151369332ae3116356eae259398
s1hagog <moshakalex@gmail.com>
2020-01-29 23:06:58 -0800

    added navbar for faster navigation (dev)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // import './App.css';
-
+import Navbar from './components/navbar.component';
 import CreateProject from './components/create-project.component';
 import CreateUser from './components/create-user.component';
 import EditProject from './components/edit-project.component';
@@ -12,10 +12,13 @@ import ProjectsList from './components/projects-list.component';
 function App() {
   return (
     <Router>
-      <Route path="/" exact component={ProjectsList} />
-      <Route path="/edit/:id" component={EditProject} />
-      <Route path="/create" component={CreateProject} />
-      <Route path="/user" component={CreateUser} />
+      <div className="container">
+        <Navbar />
+        <Route path="/" exact component={ProjectsList} />
+        <Route path="/edit/:id" component={EditProject} />
+        <Route path="/create" component={CreateProject} />
+        <Route path="/user" component={CreateUser} />
+      </div>
     </Router>
   );
 }
---
+++ src/components/navbar.component.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+
+const Navbar = () => {
+
+
+    return (
+        <nav className="navbar navbar-dark bg-dark navbar-expand-md">
+            <Link className="navbar-brand" to="/">Home</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/edit/:id">EditProjects</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/create">CreateProject</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/user">CreateUser</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
