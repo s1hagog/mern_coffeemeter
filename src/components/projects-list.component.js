7e14de59e1a4483ea79be147d04ed46d6ab031bf
s1hagog <moshakalex@gmail.com>
2020-01-31 08:34:18 -0800

    project list data fetching

--- src/components/projects-list.component.js
+++ src/components/projects-list.component.js
@@ -1,6 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 export default class ProjectsList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.deleteExerice = this.deleteExerice.bind(this);
+
+        this.state = {exercises: []};
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:5000/projects/')
+            .then(res => {
+                this.setState({exercises: res.data});
+            }).catch(err => console.log(err));
+    }
+
+    deleteExerice(id){
+        axios.delete(`http://localhost:5000/projects/${id}`)
+            .then(res => {
+                console.log(res.data);
+            });
+        this.setState({exercises: this.state.exercises.filter(el => el._id !== id)})
+    }
+
+
     render() {
         return (
             <div>
