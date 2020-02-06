f68618dd8b52f82e7ae0794ef42bf976554cdd11
s1hagog <moshakalex@gmail.com>
2020-02-06 07:57:58 -0800

    added on image click

--- src/components/projects-list.component.js
+++ src/components/projects-list.component.js
@@ -10,7 +10,9 @@ const ProjectThumbnail = (props) => {
     return (
         <div className="col-md-4">
           <div className="card mb-4 shadow-sm">
-            <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">{name}</text></svg>
+            <Link to={`/coffees/${_id}`}>
+                <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">{name}</text></svg>
+            </Link>
             <div className="card-body">
               <p className="card-text">{description}</p>
               <p className="card-text">Current coffees: {coffeesAmount}</p>
