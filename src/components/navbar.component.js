116e5098d95d3b7b8cbf30e37d9eabeb76e500bb
s1hagog <moshakalex@gmail.com>
2020-06-11 19:06:59 -0700

    fixed typo

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -38,7 +38,7 @@ const Navbar = () => {
                         }
                     </li>
                     <li className="nav-item">
-                        {<Link className="nav-link" to="/">Admin Panel</Link>
+                        <Link className="nav-link" to="/">Admin Panel</Link>
                     </li>
                 </ul>
             </div>
