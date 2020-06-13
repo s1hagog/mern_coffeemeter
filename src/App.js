59415c2dfb02d0bdb0f4411c6dce7a1852a496d4
s1hagog <moshakalex@gmail.com>
2020-06-12 19:15:35 -0700

    createde tag

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
         <Route path="/test" component={TestRoute} />
         <Route path="/account/sign-out" component={SignOutUser} />
         <Route path="/forgot-password" component={ForgotPassword} />
-
+        <footer>Developed by Alexander Moshak</footer>
       </div>
 
     </Router>
