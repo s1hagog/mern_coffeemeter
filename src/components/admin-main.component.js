e8f3981521b4ed04f5dd24f33d96250b39c9df88
s1hagog <moshakalex@gmail.com>
2020-06-15 19:38:54 -0700

    fix

--- src/components/admin-main.component.js
+++ src/components/admin-main.component.js
@@ -2,3 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { getFromStrorage, setInStorage } from '../utils/storage';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+
+const AdminMain = () => {
+
+}
