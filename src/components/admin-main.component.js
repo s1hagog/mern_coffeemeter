3f4d13466b970414fa03d7c86102d0b3bc555f56
s1hagog <moshakalex@gmail.com>
2020-06-17 20:06:31 -0700

    what is happeing

--- src/components/admin-main.component.js
+++ src/components/admin-main.component.js
@@ -29,7 +29,7 @@ const AdminMain = () => {
 
     return (
         <div className="alert alert-primary" role="alert">
-            This is a test component!
+            This is a test component! WHYYYY
         </div>
     );
 }
